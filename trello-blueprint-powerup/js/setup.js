5606781f4e193ffb22fb6be0c8535e7bf9a40f16
Daragh O'Shea <daraghoshea@gmail.com>
2017-01-23 03:13:53 +0000

    test t.getAll()

--- trello-blueprint-powerup/js/setup.js
+++ trello-blueprint-powerup/js/setup.js
@@ -12,6 +12,11 @@ var blueprintMap =  {
 };
 
 var updateCard = function(t, options) {
+  return t.getAll()
+    .then(function(){
+      debugger;
+    });
+
   return t.get('card', 'shared', 'type', '')
     .then(function(type){
         if(type == '' || Object.keys(blueprintMap).indexOf(type.toLowerCase()) < 0) {
