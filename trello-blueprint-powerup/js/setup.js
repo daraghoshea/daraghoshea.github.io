43e61631bf63eea58ccd30c37cf52bc5385fe490
Daragh O'Shea <daraghoshea@gmail.com>
2017-01-23 00:38:02 +0000

    test card button

---
+++ trello-blueprint-powerup/js/setup.js
@@ -0,0 +1,44 @@
+var blueprintMap =  {
+  'tp': 'Touchpoint',
+  'ac': 'Actor', 
+  'cr': 'Critical', 
+  'da': 'Data',
+  'id': 'Idea', 
+  'ob': 'Observation',
+  'po': 'Policy', 
+  'qu': 'Question', 
+  'st': 'Stakeholder', 
+  'sy': 'System'
+};
+
+var cardButtonCallback = function(t){
+  var items = Object.keys(blueprintMap).map(function(bp){
+    return {
+      text: blueprintMap[bp],
+      callback: function(t){
+        console.log(t)
+      }
+    };
+  });
+
+  return t.popup({
+    title: 'Blueprint Types',
+    items: items,
+    search: {
+      count: 5,
+      placeholder: 'Search Item Types',
+      empty: 'No items found'
+    }
+  });
+};
+
+
+TrelloPowerUp.initialize({
+  'card-buttons': function(t, options) {
+    return [{
+      // icon: GRAY_ICON,
+      text: 'Blueprint',
+      callback: cardButtonCallback
+    }];
+  },
+});
